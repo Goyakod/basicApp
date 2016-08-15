c2c0bd3b90a28516aab402f6994f8e16083f9713
vicky_snow <635214208@qq.com>
2016-08-15 17:17:52 +0800

    empty project commit

---
+++ basicApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  basicApp
+//
+//  Created by pro on 16/8/15.
+//  Copyright © 2016年 goyakod. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
